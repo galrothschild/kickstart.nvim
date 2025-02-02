59d7ba5e77d03d8f52186179ee5cf79c24968858
Gal Rothschild <gal.rothschildf+git@gmail.com>
2025-02-02 19:46:00 +0200

    making biome have a default and autostart

--- lua/custom/plugins/biome.lua
+++ lua/custom/plugins/biome.lua
@@ -14,6 +14,24 @@ return {
               end,
             })
           end,
+          autostart = true,
+          settings = {
+            biome = {
+              config(function()
+                local project_root = vim.fn.getcwd()
+                local biome_config = project_root .. '/biome.json'
+                local default_biome = vim.fn.expand '~/biome.json'
+
+                if vim.fn.filereadable(biome_config) == 1 then
+                  return biome_config
+                elseif vim.fn.filereadable(default_biome) == 1 then
+                  return default_biome
+                else
+                  return nil
+                end
+              end)(),
+            },
+          },
         },
       },
     },
